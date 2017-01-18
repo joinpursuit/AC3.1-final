611cf7276a439b9f9523ba3535fa5287c0d62b83
kaladin9017 <sanchochris@gmail.com>
2017-01-18 16:52:05 -0500

    Finished

--- coding-problems.js
+++ coding-problems.js
@@ -5,32 +5,68 @@
 /////
 //#1
 //Write a function called `sumArray` that takes in an array (that could contain any type of data) and returns a sum of the numbers in the array.
-const sumArray = () => null;
+const sumArray = (arr) => {
+  let sum = 0
+  arr.forEach(val => {
+    typeof val == 'number' ? sum += val : null
+  });
+  return sum
+};
 
 /////
 //#2
 //Write a function called `sumAll` that takes in any amount of numbers (not in an array), and returns a sum of all of the numbers
-const sumAll = () => null;
+const sumAll = (...args) => {
+  let sum = 0;
+  for(let i=0; i< args.length; i++) {
+    sum += args[i];
+  }
+  return sum;
+};
 
 /////
 //#3
 // Write a function called 'squaredTimesTen' that takes in array (that could contain any type of data), squares the numbers and multiples them by 10, and returns a new array of those numbers
-const squaredTimesTen = () => null;
+const squaredTimesTen = (arr) => {
+  let res = []
+  arr.forEach(val => {
+    typeof val == 'number' ? res.push((val*val)*10): null;
+  });
+  return res
+};
 
 /////
 //#4
 //Write a function called 'highLow' that takes in an array of numbers and returns an object with both the highest and lowest number from the array
-const highLow = () => null;
+const highLow = (arr) => {
+  let low = arr[0]
+  let high = arr[0]
+
+  arr.forEach(val => {
+    val > high ? high = val : val < low ? low = val : null
+  });
+  return {high, low}
+};
 
 /////
 //#5
 //Write a function called `indexMap` that takes in an array of numbers and returns a new array with each of the original numbers multiplied by their array index.
-const indexMap = () => null;
+const indexMap = (arr) => {
+  return arr.map((val, idx) => {
+    return val * idx
+  });
+};
 
 /////
 //#6
 //Write a function `oddNumStrs` that receives an array of strings and numbers and returns an array that only contains the strings from the original array that have an odd number of letters.
-const oddNumStrs = () => null;
+const oddNumStrs = (arr) => {
+  let res = []
+  arr.forEach(val => {
+     typeof val === 'string' && val.length % 2 !== 0 ? res.push(val) : null;
+  });
+  return res;
+};
 
 /////
 //#7
@@ -50,7 +86,11 @@ const oddNumStrs = () => null;
     </body>
 </html>`
 
-const changeBearPig = () => null;
+const changeBearPig = () => {
+  // $('.text1').innerHTML =
+  // let dog = document.getElementById('text1');
+  // console.log(dog)
+};
 
 /////
 //#8
@@ -58,12 +98,37 @@ const changeBearPig = () => null;
 //For each number in the array, if the number is in the first half of the array, divide it by ten
 //if the number in the second half of the array, multiply it by ten.
 //If it's exactly in the middle, leave the number alone. You should return a new array.
-const halfsies = () => null;
+const halfsies = (arr) => {
+  let len = arr.length;
+  let res =[];
+
+  if((len/2) % 2 == 0) {
+    arr.forEach((val,idx) => {
+      idx < (len/2) ? res.push(val/10): res.push(val*10);
+    })
+  }
+  else {
+    arr.forEach((val,idx) => {
+      idx < ((len/2) -1 + .5) ? res.push(val/10): idx === ((len/2) -1 + .5)? res.push(val): res.push(val*10);
+      console.log(idx, ((len/2) -1 + .5))
+    });
+  }
+
+
+  return res
+};
 
 /////
 //#9
-//Write a function called `leet` that takes in a string and returns a new string that replaces all e's with 3's, l's with 1's, and t's with 7's
-const leet = () => null;
+//Write a function called `leet` that takes in a string and returns a new string
+// that replaces all e's with 3's, l's with 1's, and t's with 7's
+const leet = (str) => {
+  let res =''
+  for(let i =0; i<str.length; i++) {
+    str[i] == 'e' ? res+= '3': str[i] == 'l'? res += '1': str[i] == 't'? res += '7' : res += str[i];
+  }
+  return res
+};
 
 /////
 //#10
@@ -71,8 +136,13 @@ const leet = () => null;
 //Each song should have an artist, title, and year property
 //as well as a 'play' prototype method that should return a string that says 'Now playing' with the name and artist of the song
 
-function Song(){
-
+function Song(artist, title, year){
+  this.artist = artist;
+  this.title = title;
+  this.year = year;
+  this.play = () => {
+    return `Now playing ${title} by ${artist}`
+  }
 };
 
 /////
@@ -81,28 +151,72 @@ function Song(){
 //Each album should have an artist, title, and year property, and an array to hold songs (which should initially start out empty).
 //It should also have an `addSong` prototype method that takes in a song object as an argument, and adds it to the album's song array.
 //Lastly, it should have a 'tracklist' prototype method that should return a single string with a title of all of the songs, separated by a comma and a space
-function Album(){
-
+function Album(artist, title, year, songs =[]){
+  this.artist = artist;
+  this.title = title;
+  this.year = year;
+  this.songs = songs;
+  this.addSong = (song) => {
+    songs.push(song)
+  }
+  this.tracklist = () => {
+    return songs.map(song => {
+      return song.title
+    }).join(', ')
+  }
 };
 
 /////
 //#12
 //Write a function called 'oddsAndEvens' that takes in an array of numbers and returns an object with a count of the number of odd and even numbers in the original array
-const oddsAndEvens = () => null;
+const oddsAndEvens = (arr) => {
+  let obj = {odds: 0, evens: 0};
+
+  arr.forEach(val => {
+    val % 2 ==0 ? obj.evens += 1: obj.odds += 1;
+  })
+  return obj;
+};
 
 /////
 //#13
 //Write a function called 'palindrome' that takes in a string and returns true if it's a palindrome, and false if not
 //(a palindrome is a word that reads the same both backwards and forwards)
 //Do NOT use the built in .reverse method
-const palindrome = () => null;
+//  racecar
+const palindrome = (str) => {
+  let end = str.length -1;
+  let res = true;
+
+  for (var i = 0; i < str.length; i++) {
+    str[i] == str[end] ? null: res = false
+    end -=1;
+  }
+  return res
+};
 
 /////
 //#14
 //Write a function called 'anagram' that takes in two strings and returns true if they are anagrams of each other, and false if not
 //(an anagram is a pair of words or sentences that can both be made by rearranging the same set of letters)
 //Do NOT use the built in .sort method
-const anagram = () => null;
+const anagram = (strOne, strTwo) => {
+  let objOne = {};
+  let objTwo = {};
+  let res = true;
+  for(let i =0; i< strOne.length; i ++) {
+    let x = strOne[i];
+    x === ' '? null : objOne[x] ? objOne[x] += 1: objOne[x] = 1
+  }
+  for(let i =0; i< strTwo.length; i ++) {
+    let x = strTwo[i];
+    x === ' '? null : objTwo[x] ? objTwo[x] += 1: objTwo[x] = 1
+  }
+  for (var key in objOne) {
+    objOne[key] !== objTwo[key] ? res = false : null
+  }
+  return res
+};
 
 
 
--- short-answers.md
+++ short-answers.md
@@ -3,15 +3,31 @@ For the following five questions, type out your answers in 1-2 paragraphs (no co
 
 1. Why is it generally considered a bad practice to create global variables?
 Answer:
+  You can unintentionally cause side effects. If you mutate some variable that was declared
+  somewhere globally and not in your local scope that function becomes an impure function and
+  the global variable now it becomes more difficult to isolate the function and debug any possible errors.
 
 1. What is closure? Give an example of where closure is used in JavaScript.
 Answer:
 
+  Closure is a technique used in javascript to save/freeze the
+  scope of a function. This allows you to access the scope of that function
+  in the future and well outside of that function.
+
 1. What is scope? When is new scope created in JavaScript?
 Answer:
 
+  Scope is an environment created when a function is declared. After a function declaration
+  any code declared or executed within that function belongs to its scope and has access to the functions' parent scope.
+
 1. What are the pros and cons of using Promises instead of callbacks?
 Answer:
+  A major Pro of using Promises instead of callbacks is having a value not undefined.
+  The function using the callback will return before the callback returns a value causing your
+  code to break. A con of using Promises instead of callbacks is a callback is short and
+  straight to the point while Promise has some setup required including testing.
 
 1. What happens when you type a URL into a browser and hit enter?
 Answer:
+  Your browser sends a request to some other server where that url is hosted and
+  if it exists you get back a 200 response and some file which is rendered to your browser.
