c212c13ec75affc24daed3d381fde80dfd5efc1d
Gabemb <gabrielbny@gmail.com>
2017-01-18 17:29:14 -0500

    ayy i finished woo

--- coding-problems.js
+++ coding-problems.js
@@ -5,32 +5,52 @@
 /////
 //#1
 //Write a function called `sumArray` that takes in an array (that could contain any type of data) and returns a sum of the numbers in the array.
-const sumArray = () => null;
+const sumArray = (arr) => arr.reduce( (sum, val) => {
+  typeof val === "number" && !isNaN(val) ? sum += val : null;
+  return sum;
+}, 0);
 
 /////
 //#2
 //Write a function called `sumAll` that takes in any amount of numbers (not in an array), and returns a sum of all of the numbers
-const sumAll = () => null;
+const sumAll = (...args) => {
+  let sum = 0
+  for(let i = 0; i < args.length; i++){
+    sum += args[i];
+  };
+  return sum;
+};
 
 /////
 //#3
 // Write a function called 'squaredTimesTen' that takes in array (that could contain any type of data), squares the numbers and multiples them by 10, and returns a new array of those numbers
-const squaredTimesTen = () => null;
+const squaredTimesTen = (arr) => arr.reduce( (newArr, val) => {
+  if (typeof val === "number" && !isNaN(val)){
+    newArr.push( Math.pow(val, 2) * 10 );
+  }
+  return newArr;
+}, []);
 
 /////
 //#4
 //Write a function called 'highLow' that takes in an array of numbers and returns an object with both the highest and lowest number from the array
-const highLow = () => null;
+const highLow = (numArr) => numArr.reduce( (highLow, num) => {
+  num > highLow.high ? highLow.high = num : null;
+  num < highLow.low ? highLow.low = num : null;
+  return highLow;
+}, {high: numArr[0], low: numArr[0]});
+
+
 
 /////
 //#5
 //Write a function called `indexMap` that takes in an array of numbers and returns a new array with each of the original numbers multiplied by their array index.
-const indexMap = () => null;
+const indexMap = (numArr) => numArr.map( (num, idx) => num * idx );
 
 /////
 //#6
 //Write a function `oddNumStrs` that receives an array of strings and numbers and returns an array that only contains the strings from the original array that have an odd number of letters.
-const oddNumStrs = () => null;
+const oddNumStrs = (strNumArr) => strNumArr.filter( (val) => typeof val === "string" && val.length % 2 !== 0 );
 
 /////
 //#7
@@ -50,7 +70,10 @@ const oddNumStrs = () => null;
     </body>
 </html>`
 
-const changeBearPig = () => null;
+const changeBearPig = () => {
+  let dog = document.getElementById('text1');
+  dog.innerText = 'ManBearPig';
+};
 
 /////
 //#8
@@ -58,12 +81,43 @@ const changeBearPig = () => null;
 //For each number in the array, if the number is in the first half of the array, divide it by ten
 //if the number in the second half of the array, multiply it by ten.
 //If it's exactly in the middle, leave the number alone. You should return a new array.
-const halfsies = () => null;
+const halfsies = (numArr) => numArr.map( (num, idx) => {
+  let length = numArr.length
+  if (numArr.length % 2 === 0) {
+    if (idx + 1 <= length / 2 ){
+     return num / 10;
+    };
+    if (idx + 1 > length / 2){
+      return num * 10;
+    }
+  } else {
+    if (idx + 1 < (length + 1) / 2) {
+     return num / 10;
+    };
+    if (idx + 1 > (length + 1) / 2){
+      return num * 10;
+    }
+    if (idx + 1 === (length + 1) / 2) {
+      return num;
+    } 
+  }
+});
 
 /////
 //#9
 //Write a function called `leet` that takes in a string and returns a new string that replaces all e's with 3's, l's with 1's, and t's with 7's
-const leet = () => null;
+const leet = (str) => str.split('').map( (letter) => {
+  switch(letter){
+    case 'e':
+      return 3;
+    case 'l':
+      return 1;
+    case 't':
+      return 7;
+    default:
+      return letter;
+  };
+}).join('');
 
 /////
 //#10
@@ -71,9 +125,15 @@ const leet = () => null;
 //Each song should have an artist, title, and year property
 //as well as a 'play' prototype method that should return a string that says 'Now playing' with the name and artist of the song
 
-function Song(){
-
+function Song(artist, title, year){
+  this.artist = artist;
+  this.title = title;
+  this.year = year;
 };
+
+Song.prototype.play = function () {
+  return 'Now playing ' + this.title + " by " + this.artist;
+}
 
 /////
 //#11
@@ -81,28 +141,74 @@ function Song(){
 //Each album should have an artist, title, and year property, and an array to hold songs (which should initially start out empty).
 //It should also have an `addSong` prototype method that takes in a song object as an argument, and adds it to the album's song array.
 //Lastly, it should have a 'tracklist' prototype method that should return a single string with a title of all of the songs, separated by a comma and a space
-function Album(){
-
+function Album(artist, title, year){
+  this.artist = artist;
+  this.title = title;
+  this.year = year;
+  this.songs = [];
 };
+
+Album.prototype.addSong = function (Song) {
+  this.songs.push(Song);
+}
+
+Album.prototype.tracklist = function () {
+  return this.songs.reduce( (track, Song, idx, songs) => {
+    if (idx === songs.length - 1) return track + Song.title;
+    return track + Song.title + ", ";
+  }, "");
+}
 
 /////
 //#12
 //Write a function called 'oddsAndEvens' that takes in an array of numbers and returns an object with a count of the number of odd and even numbers in the original array
-const oddsAndEvens = () => null;
+const oddsAndEvens = (numArr) => numArr.reduce( (hash, num) => {
+  num % 2 === 0 ? hash.evens++ : hash.odds++;
+  return hash;
+}, {odds: 0, evens: 0});
 
 /////
 //#13
 //Write a function called 'palindrome' that takes in a string and returns true if it's a palindrome, and false if not
 //(a palindrome is a word that reads the same both backwards and forwards)
 //Do NOT use the built in .reverse method
-const palindrome = () => null;
+const palindrome = (str) => {
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  };
+  return reverse === str ? true : false;
+};
 
 /////
 //#14
 //Write a function called 'anagram' that takes in two strings and returns true if they are anagrams of each other, and false if not
 //(an anagram is a pair of words or sentences that can both be made by rearranging the same set of letters)
 //Do NOT use the built in .sort method
-const anagram = () => null;
+const anagram = (str1, str2) => {
+  let result = true;
+
+  hash1 = str1.split('').reduce( (hash, lett) => {
+    hash[lett] ? hash[lett] += 1 : hash[lett] = 1;
+    return hash;
+  },{});
+
+  hash2 = str2.split('').reduce( (hash, lett) => {
+    hash[lett] ? hash[lett] += 1 : hash[lett] = 1;
+    return hash;
+  },{});
+
+  for (let key in hash1){
+    if(hash1[key] !== hash2[key]){
+      key !== ' ' ?
+      result = false
+      :
+      null;
+    };
+  };
+
+  return result;
+};
 
 
 
@@ -117,6 +223,7 @@ const anagram = () => null;
 
 
 //Ignore this:
+//DONT TELL ME WHAT TO DO
 module.exports = {
   sumArray,
   sumAll,
--- short-answers.md
+++ short-answers.md
@@ -2,16 +2,23 @@
 For the following five questions, type out your answers in 1-2 paragraphs (no coding required):
 
 1. Why is it generally considered a bad practice to create global variables?
-Answer:
 
-1. What is closure? Give an example of where closure is used in JavaScript.
-Answer:
+Answer: Global variables created in functions can interfere with code outside of the function and start messing with all sort of stuff. You might end up accidentally overwriting data as well. It can make your code inconsistent and inaccurate.
 
-1. What is scope? When is new scope created in JavaScript?
-Answer:
+2. What is closure? Give an example of where closure is used in JavaScript.
 
-1. What are the pros and cons of using Promises instead of callbacks?
-Answer:
+Answer: A closure is when a function is declared inside of another function and has access to all the "parent"'s functions variables and arguments. A closure can be a nice way of getting the same utility of a global variable while not actually making a variable global. It can be handy for things like recursion, where you'd want to keep a variable in the parent function while the actual recursion happens in the inner function. This way the value of the variable doesn't revert with each new recursive function call.
 
-1. What happens when you type a URL into a browser and hit enter?
-Answer:
+3. What is scope? When is new scope created in JavaScript?
+
+Answer: Scope describes what variables and values are accessible by which functions. New scope is created with each new function declaration, the curly braces of a function serving as the borders (although in ES6, `let` and `const` allow you declare new scope within each "block", a block being anything within curly braces {}. The variables inside for loops can now be contained within just that for loop declaration, for example). All variables and values inside of a parent function's scope are accessible by it's "children", but not vice-versa.
+
+4. What are the pros and cons of using Promises instead of callbacks?
+
+Answer: Since in JavaScript most actions will happen independently of the each other, asynchronously, things can get annoying when you want the value of one action before running another action. Promises are nice for this since they essentially will not run until whatever value you want from some initial action returns. This is especially nice for backend server and database stuff where you usually want a certain flow of actions happening in a certain order usually passing info down to each other. Promises are also nice because you don't get that ugly look of nested callbacks within callbacks creating a unreadable slope of callback hell.
+
+However, with callbacks you could do multiple things at once, allowing you to do things like maybe render something to a page even though every function for that page may not be done running yet.
+
+5. What happens when you type a URL into a browser and hit enter?
+
+Answer: You are essentially making a GET request with your browser to whatever address you just typed in. First the request is handled by your IP provider which forwards the request to some sort of DNS server somewhere tht acts like a sort of directory where it looks for the domain name you've requested and then forwards the request to the appropriate IP address attached to that domain name leading the server of the actual website you've requested. The site server listening for '/' will then fetch whatever data or response that server is set to respond with in case of GET requests to '/'. It then sends that data or response back to your computer where it's, usually, displayed on your screen. (assuming the website and server both exist and/or are currently working).
