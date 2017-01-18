ce1ba3b6d85c22ecc455d6511cfc2530a4bffd4f
Quan <QuanAVuong@users.noreply.github.com>
2017-01-18 18:04:29 -0500

    initial commit

--- coding-problems.js
+++ coding-problems.js
@@ -5,32 +5,40 @@
 /////
 //#1
 //Write a function called `sumArray` that takes in an array (that could contain any type of data) and returns a sum of the numbers in the array.
-const sumArray = () => null;
+const sumArray = arr => arr.reduce( (sum, cur) => sum += (typeof cur === "number") ? cur : 0 );
 
 /////
 //#2
 //Write a function called `sumAll` that takes in any amount of numbers (not in an array), and returns a sum of all of the numbers
-const sumAll = () => null;
+const sumAll = nums => [...nums].reduce( (sum, cur) => sum += cur ) ;
 
 /////
 //#3
 // Write a function called 'squaredTimesTen' that takes in array (that could contain any type of data), squares the numbers and multiples them by 10, and returns a new array of those numbers
-const squaredTimesTen = () => null;
+const squaredTimesTen = arr => arr.filter( ele => {
+    if (typeof ele === "number") return ele*ele*10
+  }
+);
 
 /////
 //#4
 //Write a function called 'highLow' that takes in an array of numbers and returns an object with both the highest and lowest number from the array
-const highLow = () => null;
+const highLow = arr => {
+  let obj = {};
+  obj["high"] = Math.max(...arr);
+  obj["low"] = Math.min(...arr);
+  return obj;
+};
 
 /////
 //#5
 //Write a function called `indexMap` that takes in an array of numbers and returns a new array with each of the original numbers multiplied by their array index.
-const indexMap = () => null;
+const indexMap = arr => arr.map( (ele, idx) => ele * idx);
 
 /////
 //#6
 //Write a function `oddNumStrs` that receives an array of strings and numbers and returns an array that only contains the strings from the original array that have an odd number of letters.
-const oddNumStrs = () => null;
+const oddNumStrs = arr => arr.filter( ele => (typeof ele === "string") && (ele.length % 2 === 1) ? ele : null);
 
 /////
 //#7
@@ -50,7 +58,9 @@ const oddNumStrs = () => null;
     </body>
 </html>`
 
-const changeBearPig = () => null;
+const changeBearPig = () => document.getElementById(text1).innerHTML("ManBearPig");
+// textContent may work as well ?
+// const changeBearPig = () => document.getElementById(text1).textContent("ManBearPig");
 
 /////
 //#8
@@ -58,12 +68,43 @@ const changeBearPig = () => null;
 //For each number in the array, if the number is in the first half of the array, divide it by ten
 //if the number in the second half of the array, multiply it by ten.
 //If it's exactly in the middle, leave the number alone. You should return a new array.
-const halfsies = () => null;
+const halfsies = arr => {
+
+  let midIndex = Math.floor( arr.length/2 );
+  
+  if (arr.length % 2 === 1) { // odd length
+    // arr.map( (num, idx) => idx < midIndex ? num/10 : idx > midIndex ? num*10 : null )
+    return arr.map( (num, idx) => {
+      if( idx < midIndex ) return num/10;
+      else { if (idx > midIndex ) return num*10 }
+    })
+  } 
+
+  else {
+    if (arr.length % 2 === 0) { // even length
+      return arr.map( (num, idx) => {
+        if( idx < midIndex ) return num/10;
+        else { if (idx >= midIndex ) return num*10 }
+      })
+    }
+  }
+
+};
 
 /////
 //#9
 //Write a function called `leet` that takes in a string and returns a new string that replaces all e's with 3's, l's with 1's, and t's with 7's
-const leet = () => null;
+const leet = str => {
+  return str.split("").map( char => {
+    let charLower = char.toLowerCase();
+    switch(charLower) {
+      case "e": return "3"; break;
+      case "l": return "1"; break;
+      case "t": return "7"; break;
+      default: return char;
+    }
+  }).join("")
+};
 
 /////
 //#10
@@ -71,9 +112,15 @@ const leet = () => null;
 //Each song should have an artist, title, and year property
 //as well as a 'play' prototype method that should return a string that says 'Now playing' with the name and artist of the song
 
-function Song(){
-
+function Song( artist, title, year ){
+  this.artist = artist;
+  this.title = title;
+  this.year = year;
 };
+
+Song.prototype.play = function() {
+  return "Now playing " + this.title + " by " + this.artist;  
+}
 
 /////
 //#11
@@ -81,14 +128,32 @@ function Song(){
 //Each album should have an artist, title, and year property, and an array to hold songs (which should initially start out empty).
 //It should also have an `addSong` prototype method that takes in a song object as an argument, and adds it to the album's song array.
 //Lastly, it should have a 'tracklist' prototype method that should return a single string with a title of all of the songs, separated by a comma and a space
-function Album(){
-
+function Album(artist, title, year){
+  this.artist = artist;
+  this.title = title;
+  this.year = year;
+  this.tracks = [];
 };
+
+Album.prototype.addSong = function(song) {
+  this.tracks.push(song);
+}
+
+Album.prototype.tracklist = function() {
+  return this.tracks.join(", ") 
+  // + this.tracks[this.tracks.length-1] ? 
+}
+
+
 
 /////
 //#12
 //Write a function called 'oddsAndEvens' that takes in an array of numbers and returns an object with a count of the number of odd and even numbers in the original array
-const oddsAndEvens = () => null;
+const oddsAndEvens = arr => {
+  let obj = {odds: 0, evens: 0}
+  arr.forEach( num => num % 2 === 0 ? obj.evens++ : obj.odds++ )
+  return obj;
+};
 
 /////
 //#13
@@ -102,7 +167,12 @@ const palindrome = () => null;
 //Write a function called 'anagram' that takes in two strings and returns true if they are anagrams of each other, and false if not
 //(an anagram is a pair of words or sentences that can both be made by rearranging the same set of letters)
 //Do NOT use the built in .sort method
-const anagram = () => null;
+const anagram = (str1, str2) => {
+  let obj1 = {};
+  let obj2 = {};
+
+  str1.split().
+};
 
 
 
--- short-answers.md
+++ short-answers.md
@@ -1,17 +1,56 @@
 # Short Answer Questions
 For the following five questions, type out your answers in 1-2 paragraphs (no coding required):
 
-1. Why is it generally considered a bad practice to create global variables?
+###1. Why is it generally considered a bad practice to create global variables?
+Answer:
+- In terms of object oriented programming (?), encapsulation (?) or the general concept of black-boxing in programming involves information hiding: it is recommended that a function/object make only its most essential part(s) available to other programs accessing it.
+- A global variable is made available to the entire program. Any function accessing a global variable cannot have full control over it, as it maybe accessed/modified by other functions as well
+
+
+###2. What is closure? Give an example of where closure is used in JavaScript.
+Answer:
+- Variables defined inside a function only exists for the duration of the function call, after which they are removed from the execution environment. 
+- A closure is a function that keeps its variables in the execution environment even after the function finishes running, in effect 'freezes' its variables, making them available to functions nested inside.
+- A thunk in JavaScript is an example of a closure
+
+
+###3. What is scope? When is new scope created in JavaScript?
+Answer:
+- Scope is the environment in which variables are made available to any operation using it.
+- A scope is created whenever code is run. 
+
+Global Scope: available throughout the program
+	Eg: 
+```javascript
+var globalVar = 10;
+function secretFunc() {
+	console.log("logging globalVar:", globalVar) // 10;
+} 
+```
+Local Scope: available locally in the scope it was defined
+```javascript
+const func1 = () => let func1Var = "func1";
+const func2 = () => func1Var ? let func2Var = func1Var : "func1Var is: " + func1Var;
+func1(); // 'func1'
+func2(); // 'func1Var is undefined' 
+```
+
+
+###4. What are the pros and cons of using Promises instead of callbacks?
+Answer:
+Pros: 
+	- to avoid callbacks multi-level-nested inside callbacks aka. "Callback Hell"
+	- easier to follow data flow
+Cons: 
+	- not all promises implementation are created equal
+
+
+
+###5. What happens when you type a URL into a browser and hit enter?
 Answer:
 
-1. What is closure? Give an example of where closure is used in JavaScript.
-Answer:
-
-1. What is scope? When is new scope created in JavaScript?
-Answer:
-
-1. What are the pros and cons of using Promises instead of callbacks?
-Answer:
-
-1. What happens when you type a URL into a browser and hit enter?
-Answer:
+- The browser will convert the URL into a corresponding IP address via DNS lookup (Domain Name Server ?)
+- The router uses the converted public IP address to look for the server that contains the data requested (eg. the web page). This may involve hopping through a network of routers around the world until the final destination.
+- When the server is reached with a HTTP Request, programs run on the server aka APIs (Application Programming Interfaces) will respond according to (in this case the default GET ) HTTP Request method.
+- The API mounts a function (middleware) defined at this particular endpoint: get all data from the database: eg. index.html, all posts etc.
+- All these data are sent via packets traversing the entire stacks (eg. application layer, transpotation, physical etc. ) until it is rendered by the browser to the user.
