00d0726345d4a930ab9036df0db937a5ea85a3d6
Crazy Kiwi <Nasita-Haque>
2017-01-18 18:01:53 -0500

    Test Completed

--- coding-problems.js
+++ coding-problems.js
@@ -5,32 +5,73 @@
 /////
 //#1
 //Write a function called `sumArray` that takes in an array (that could contain any type of data) and returns a sum of the numbers in the array.
-const sumArray = () => null;
+const sumArray = (arr) => {
+  let count = 0
+  for(let i = 0; i < arr.length; i++){
+    if(typeof arr[i] === 'number'){
+      count += arr[i]
+    }
+  }
+  return count; 
+};
 
 /////
-//#2
+//#2 
 //Write a function called `sumAll` that takes in any amount of numbers (not in an array), and returns a sum of all of the numbers
-const sumAll = () => null;
+const sumAll = (...args) => {
+  let count = 0
+  for(let i = 0; i < args.length; i++){
+    count += args[i]
+  }
+  return count;
+};
 
 /////
 //#3
 // Write a function called 'squaredTimesTen' that takes in array (that could contain any type of data), squares the numbers and multiples them by 10, and returns a new array of those numbers
-const squaredTimesTen = () => null;
+const squaredTimesTen = (arr) => {
+  let newArr = []
+  for(let i = 0; i < arr.length; i++){
+    if(typeof arr[i] === 'number'){
+      newArr.push((arr[i]*arr[i])*10)
+    }
+  }
+  return newArr
+};
 
 /////
 //#4
 //Write a function called 'highLow' that takes in an array of numbers and returns an object with both the highest and lowest number from the array
-const highLow = () => null;
+const highLow = (arr) => {
+  let obj = {high: -10000000, low: 10000000}
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > obj["high"]){
+      obj["high"] = arr[i]
+    } 
+    if(arr[i] < obj["low"]){
+      obj["low"] = arr[i]
+    }
+  }
+    return obj; 
+}; 
 
 /////
 //#5
 //Write a function called `indexMap` that takes in an array of numbers and returns a new array with each of the original numbers multiplied by their array index.
-const indexMap = () => null;
+const indexMap = (arr) => {
+  return arr.map((val, idx)=>{
+    return val * idx
+  })
+};
 
 /////
 //#6
 //Write a function `oddNumStrs` that receives an array of strings and numbers and returns an array that only contains the strings from the original array that have an odd number of letters.
-const oddNumStrs = () => null;
+const oddNumStrs = (arr) => {
+  return arr.filter((val, idx)=>{
+    return typeof val === 'string' && val.length % 2 === 1
+  })
+}; 
 
 /////
 //#7
@@ -50,59 +91,173 @@ const oddNumStrs = () => null;
     </body>
 </html>`
 
-const changeBearPig = () => null;
+const changeBearPig = () => {
+  let text1 = document.getElementById('text1')
+  return text1.innerHTML = 'ManBearPig'
+};
 
 /////
-//#8
+//#8 
 //write a function called `halfsies` that takes in an array of numbers.
 //For each number in the array, if the number is in the first half of the array, divide it by ten
 //if the number in the second half of the array, multiply it by ten.
 //If it's exactly in the middle, leave the number alone. You should return a new array.
-const halfsies = () => null;
+const halfsies = (arr) => { //[100,100,100,100]
+  let final = [] 
+  let even;
+  let odd; 
+  arr.length % 2 === 0 ? even = ((arr.length/2)-1) : odd = ((arr.length-1)/2) 
+    for(let i = 0; i < arr.length; i++){
+        if(even){
+          if(i < even || i === even){
+            final.push(arr[i] / 10) 
+          } else if(i > even){
+            final.push(arr[i] * 10)
+          }
+        } else if (odd){
+          if(i < odd){
+            final.push(arr[i] / 10) 
+          } else if (i === odd){
+            final.push(arr[i])
+          } else if (i > odd){
+            final.push(arr[i] * 10)
+          }
+        }
+
+      }
+  return final 
+}; 
 
 /////
 //#9
 //Write a function called `leet` that takes in a string and returns a new string that replaces all e's with 3's, l's with 1's, and t's with 7's
-const leet = () => null;
+const leet = (str) => {
+  let replacedStr = ""
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === 'e'){
+      replacedStr += "3"
+    } else if (str[i] === 'l'){
+      replacedStr += "1"
+    } else if (str[i] === 't'){
+      replacedStr += "7"
+    } else {
+      replacedStr += str[i]
+    }
+  }
+  return replacedStr;
+};
 
 /////
-//#10
+//#10 
 //Write a constructor function called `Song` for creating 'Song' objects
 //Each song should have an artist, title, and year property
 //as well as a 'play' prototype method that should return a string that says 'Now playing' with the name and artist of the song
 
-function Song(){
-
+function Song(artist, title, year){
+  this.artist = artist
+  this.title = title
+  this.year = year
 };
 
+Song.prototype.play = function(){
+  return "Now playing " + this.title + ' by ' + this.artist
+}
+
 /////
-//#11
+//#11 
 //Write a constructor function called `Album` for creating 'Album' objects.
 //Each album should have an artist, title, and year property, and an array to hold songs (which should initially start out empty).
 //It should also have an `addSong` prototype method that takes in a song object as an argument, and adds it to the album's song array.
 //Lastly, it should have a 'tracklist' prototype method that should return a single string with a title of all of the songs, separated by a comma and a space
-function Album(){
-
+function Album(artist, title, year){
+  this.artist = artist
+  this.title = title
+  this.year = year
+  this.song = []
 };
+
+Album.prototype.addSong = function(song){
+  return this.song.push(song)
+}
+
+Album.prototype.tracklist = function(){
+  let list = ""
+  for(let i = 0; i < this.song.length; i++){
+    if(i === this.song.length-1){
+      list += this.song[i].title
+    } else {
+      list += this.song[i].title + ", " 
+    }
+  }
+  return list
+}
 
 /////
 //#12
 //Write a function called 'oddsAndEvens' that takes in an array of numbers and returns an object with a count of the number of odd and even numbers in the original array
-const oddsAndEvens = () => null;
+const oddsAndEvens = (arr) => {
+  let obj = {odds: 0, evens: 0}
+  arr.forEach((val, idx)=>{
+    val % 2 === 0 ? obj["evens"]+= 1 : obj["odds"]+= 1 
+  })
+  return obj
+};
 
 /////
 //#13
 //Write a function called 'palindrome' that takes in a string and returns true if it's a palindrome, and false if not
 //(a palindrome is a word that reads the same both backwards and forwards)
 //Do NOT use the built in .reverse method
-const palindrome = () => null;
+const palindrome = (arr) => {
+  let reverse = []
+  for(let i = arr.length-1; i > 0; i--){
+    reverse.push(arr[i])
+  }
+  for(let j = 0; j < arr.length; j++){
+    if (arr[j] !== reverse[j]){
+      return false
+    } else {
+      return true 
+    }
+  }
+};
 
 /////
 //#14
 //Write a function called 'anagram' that takes in two strings and returns true if they are anagrams of each other, and false if not
 //(an anagram is a pair of words or sentences that can both be made by rearranging the same set of letters)
 //Do NOT use the built in .sort method
-const anagram = () => null;
+const anagram = (str1, str2) => {
+  let result;
+  let string1 = ""
+  let string2 = ""
+  if(str1.length !== str2.length){
+    return false 
+  } else {
+
+  for(var j = 0; j < str1.length; j++){
+    if(str1[j] !== ''){
+      string1 += str1[j]
+    }
+  }
+  for(var g = 0; g < str2.length; g++){
+    if(str2[g] !== ''){
+      string2 += str2[g]
+    }
+  }
+    for(let i = 0; i < string1.length; i++){
+      for(let k = 0; k < string2.length; k++){
+        if(string1[i] == string2[k]){
+          result = true 
+        } else {
+          result = false
+        }
+      }
+    }
+  }
+  return result
+};
+
 
 
 
--- short-answers.md
+++ short-answers.md
@@ -3,15 +3,20 @@ For the following five questions, type out your answers in 1-2 paragraphs (no co
 
 1. Why is it generally considered a bad practice to create global variables?
 Answer:
+	It is generally considered bad practice to create global variables because creating global variables can cause variable value to change especially if it is affected by some other function. It's best practice to keep variables within the specific scope you need them for so you can keep track of all your varibales and any errors or changes in your return value. 
 
 1. What is closure? Give an example of where closure is used in JavaScript.
 Answer:
+	A closure is when a function has access to variables within its own scope and other functions that maybe wrapped within the main function. For example, if you create a function called countNumbers and then insert another function inside called countLetters. If you for example also created a variable within countNumbers, that variable will be accessible by the second function called countLetters. It creates a scope that makes the variable available to the inner functions but only within the scope of the main function. It is like creating a local global scope of the variable that can be accessed by the inner functions.
 
 1. What is scope? When is new scope created in JavaScript?
 Answer:
+	Scope is the accessibility of a variable. There are two kinds of scope, global and local. Global scope means that a variable is accessible by any function anywhere. Local scope means that the variable is only available in the function or context in which it was defined in. 
 
 1. What are the pros and cons of using Promises instead of callbacks?
 Answer:
-
+	Promises are ways to perform asynchronous actions. One important reason why promises are used is to make sure you don't go through 'callback hell' which is when many callbacks are nested within each other. Promises are great to clarify, organize and write clean code to avoid the callback issues. On the otherhand, if you don't have many callbacks, you could use that instead of Promises because Promises can get complicated if you want to just write out a simple callback function. In that case, it would be better to write it out as a callback rather than a promise. 
+ 
 1. What happens when you type a URL into a browser and hit enter?
 Answer:
+	When a URL is typed into a browser, the URL is given a specific http request and then sent back to the server that performs the request whether it is a GET, POST, PUT, DELETE request and then sends that data back the browser. The URL is basically checking against the server side and then retrieving data to bring to the client side. 
